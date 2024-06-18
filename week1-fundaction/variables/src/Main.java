4239060569f815ae7917bc28e66f17ec36c897e2
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-06-17 21:59:49 -0300

    First commit

---
+++ week1-fundaction/variables/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String name = "Eduardo Guedes";
+        int age = 20;
+        boolean married = false;
+        char nota = 'A';
+        double price = 5.5;
+        //VERIFICAR SOBRE FLOAT
+        float  priceFloat = 5.5f;
+
+        System.out.println("System to learn in fundaction of variables: ");
+        System.out.println("String name: " + name);
+        System.out.println("int age: " + age);
+        System.out.println("boolean married: " + married);
+        System.out.println("char nota: " + nota);
+        System.out.println("double price: " + price);
+        System.out.println("float  priceFloat: " + priceFloat);
+    }
+}
