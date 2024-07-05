b86ca2690fea01858cc4d63b53c4ed6244e128c3
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-07-04 22:36:55 -0300

    Continuacao do usu do banco de dados - Comando INSERT e UPDATE implementados

--- database/oop-and-database/src/main/java/org/example/Main.java
+++ database/oop-and-database/src/main/java/org/example/Main.java
@@ -9,17 +9,42 @@ public class Main {
 
     UserDAO userDAO = new UserDAO();
 
-    System.out.println("O QUE DESEJA BUSCAR:");
-    System.out.println("1 - Usuario");
-    System.out.println("2 - Cliente");
+    System.out.println("O QUE DESEJA FAZER:");
+    System.out.println("1 - Buscar Usuarios");
+    System.out.println("2 - Cadastrar Usuarios");
+    System.out.println("2 - Alterar usuarios");
+    int opcao =  scanner.nextInt();
 
-    if(scanner.nextInt() == 1) {
+    if(opcao == 1) {
       List<User> listaUser = userDAO.getAllUsers();
 
       for (User user : listaUser) {
         System.out.println("Usuario: " + user.getName());
       }
-    } else  {
+    } else if (opcao == 2) {
+      String name, email, phone, password;
+      System.out.println("Vamos Cadastrar o novo Usuario");
+      System.out.print("Nome: ");
+      name = scanner.next();
+      System.out.print("Email: ");
+      email = scanner.next();
+      System.out.print("Phone: ");
+      phone = scanner.next();
+      System.out.print("Password: ");
+      password = scanner.next();
+
+      userDAO.insertUser(name, email, phone, password);
+    } else if(opcao == 3)  {
+      String newName, oldName;
+
+      System.out.print("Qual usuario deseja alterar: ");
+      oldName = scanner.next();
+      System.out.print("Qual novo nome: ");
+      newName = scanner.next();
+
+      userDAO.updateNameUser(newName, oldName);
+    }
+    else  {
       System.out.println("Funcao nao implementada");
     }
 
--- database/oop-and-database/src/main/java/org/example/UserDAO.java
+++ database/oop-and-database/src/main/java/org/example/UserDAO.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -8,7 +9,26 @@ import java.util.List;
 
 public class UserDAO {
 
-  public void insertUser() {}
+  public void insertUser(String name, String email, String phone, String password) {
+    String sqlInsert =
+                    " INSERT INTO public.java$user(" +
+                    " usr_name, usr_email," +
+                    " usr_phone,usr_password," +
+                    " usr_active) VALUES (?, ?, ?, ?, true);";
+
+    try(Connection conexao = Conexao.getConnection();
+        PreparedStatement pstmt = conexao.prepareStatement(sqlInsert)
+    ) {
+      pstmt.setString(1, name);
+      pstmt.setString(2, email);
+      pstmt.setString(3, phone);
+      pstmt.setString(4, password);
+
+      pstmt.executeUpdate();
+    } catch(SQLException e){
+      System.out.println("Erro ao inserir usuario: " + e.getMessage());
+    }
+  }
 
   public List<User> getAllUsers() {
     List<User> users = new ArrayList<>();
@@ -25,9 +45,23 @@ public class UserDAO {
         users.add(new User(name, email, phone, password));
       }
     } catch (SQLException e) {
-      System.out.println("Erro ao buscar usuarios: " + e.getMessage());;
+      System.out.println("Erro ao buscar usuarios: " + e.getMessage());
     }
     return users;
+  }
+
+  public void updateNameUser(String newName, String oldName) {
+    String sqlUpade = "UPDATE java$user SET usr_name = ? where usr_name = ?";
+    try(Connection conexao = Conexao.getConnection();
+        PreparedStatement prmst = conexao.prepareStatement(sqlUpade)) {
+      System.out.println(newName + " - " + oldName);
+      prmst.setString(1, newName);
+      prmst.setString(2, oldName);
+      prmst.executeUpdate();
+      System.out.println("Usuario alterado com sucesso");
+    } catch (SQLException e) {
+      System.out.println("Ocorreu um erro ao atualizar usuario: " + e.getMessage());
+    }
   }
 
 }
