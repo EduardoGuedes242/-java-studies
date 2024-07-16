de06a434b1f7101921cd4b9db414dfba1a5bc337
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-07-15 21:44:02 -0300

    add item in array

--- java-advanced/estruturasdados/src/main/java/org/example/Aula3.java
+++ java-advanced/estruturasdados/src/main/java/org/example/Aula3.java
@@ -1,5 +1,17 @@
 package org.example;
 
 public class Aula3 {
+  public static void main(String[] args) {
+    Vetor vetor = new Vetor(50000);
 
+    for (int i = 0; i < 50000; i++) {
+      vetor.addItem("Teste: " + i);
+
+    }
+
+//    for (int i = 0; i < vetor.tamanhoVetor(); i++) {
+//      System.out.println("Item na lista: " + vetor.getValorArray(i));
+//    }
+
+  }
 }
--- java-advanced/estruturasdados/src/main/java/org/example/Vetor.java
+++ java-advanced/estruturasdados/src/main/java/org/example/Vetor.java
@@ -1,19 +1,47 @@
 package org.example;
 
+import java.time.Duration;
+
 public class Vetor {
 
   private String[] elementos;
+  private int qtdRealVetor;
 
   public Vetor(int capacidade) {
     this.elementos = new String[capacidade];
+    this.qtdRealVetor = 0;
   }
 
   public int tamanhoVetor() {
     return  this.elementos.length;
   }
 
-  public void addItem(String elemento) {
 
+  //BY GUEDES
+//  public void addItem(String elemento) {
+//    for (int i = 0; qtdRealVetor < this.elementos.length; i++) {
+//      if(this.elementos[i] == null) {
+//        this.elementos[i] = elemento;
+//        qtdRealVetor++;
+//        break;
+//      }
+//    }
+//  }
+
+  //Aprendido
+  public boolean addItem(String elemento) {
+    if(this.qtdRealVetor < this.elementos.length) {
+      this.elementos[this.qtdRealVetor] = elemento;
+      this.qtdRealVetor ++;
+      return true;
+    } else {
+      return false;
+    }
+ }
+
+
+  public String getValorArray(int index) {
+    return this.elementos[index];
   }
 
 }
