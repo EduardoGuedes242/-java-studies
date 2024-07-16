bbd2b7bd9c0507fc93c98192655c241d8d31a06b
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-07-15 21:09:36 -0300

    Learn java advanced - arrays

---
+++ java-advanced/estruturasdados/src/main/java/org/example/Aula3.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Aula3 {
+
+}
---
+++ java-advanced/estruturasdados/src/main/java/org/example/Vetor.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Vetor {
+
+  private String[] elementos;
+
+  public Vetor(int capacidade) {
+    this.elementos = new String[capacidade];
+  }
+
+  public int tamanhoVetor() {
+    return  this.elementos.length;
+  }
+
+  public void addItem(String elemento) {
+
+  }
+
+}
